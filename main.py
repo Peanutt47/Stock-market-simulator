fd52eb8321918da07db4ffd9cdef5045c5ef6a2f
Peanutt47 <pnchalee@gmail.com>
2022-11-30 01:51:01 +0700

    create Trending Tickers

--- main.py
+++ main.py
@@ -5,31 +5,24 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://finance.yahoo.com/lookup"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
-x = soup.findAll("td", {"class": "data-col0 Ta(start) Pstart(6px) Pend(15px)"})
-# for i in range(len(x)):
-#     print(i + 1, x[i].text)
-open("Trending Tickers.txt", "w").write("\n".join([i.text for i in x]))
-
 user_data = User("admin", "12345", 10000)
 market = Market(user_data)
 
 
 def create_account():
     global user_data
-    print("Register")
+    print("Register Menu")
     username = input("Enter your username or (Q)uit: ")
-    password = input("Enter your password: ")
-    money = input("Enter your money: ")
     if username.lower() == "quit" or username.lower() == "q":
         main()
+    password = input("Enter your password: ")
+    money = input("Enter your money: ")
     try:
         with open("user_data.json", "r") as user_flie:
             user_data = json.load(user_flie)
     except FileNotFoundError:
         User(username, password, money).create_account()
+        print("Account registered!")
         main()
     else:
         if username in user_data:
@@ -52,8 +45,10 @@ def create_account():
 
 def login():
     global market
-    print("Login")
+    print("Login Menu")
     username = input("Enter your username or (Q)uit: ")
+    if username.lower() == "quit" or username.lower() == "q":
+        main()
     password = input("Enter your password: ")
     # if username in user_data.username:
     #     if user_data.password == password:
@@ -82,45 +77,88 @@ def login():
             else:
                 print("Incorrect password!")
                 login()
-        elif username.lower() == "quit" or username.lower() == "q":
-            main()
         elif username not in user_data:
             print("Username does not exist!")
             login()
+
+
+def trading_tickers():
+    url = "https://finance.yahoo.com/lookup"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    x = soup.findAll("td", {"class": "data-col0 Ta(start) Pstart(6px) Pend(15px)"})
+    tem = {}
+    for i in range(len(x)):
+        tem[i + 1] = x[i].text
+    with open("tickers.json", "w") as tickers:
+        json.dump(tem, tickers)
+    with open("tickers.json", "r") as tickers:
+        tickers = json.load(tickers)
+    # for i in range(len(file)):
+    #     print(f"{i+1}. {file.readline()}")
+    text = []
+    print(f"-" * 39, end="")
+    print(f"-")
+    tem_text = ""
+    for i in tickers:
+        if int(i) % 3 == 0:
+            tem_text += f"| {i}. {tickers[i]}"
+            if len(tem_text) < 39:
+                tem_text += " " * (39 - len(tem_text))
+            tem_text += "|"
+            text.append(tem_text)
+            text.append("----------------------------------------")
+            tem_text = ""
+        else:
+            tem_text += f"| {i}. {tickers[i]}"
+            if len(tem_text) < 13:
+                tem_text += " " * (13 - len(tem_text))
+            elif len(tem_text) < 26:
+                tem_text += " " * (26 - len(tem_text))
+    for i in text:
+        print(i)
 
 
 def trade_menu():
     print("Just HODL!!")
     print("1. Buy Stock")
     print("2. Sell Stock")
-    print("3. Trading stocks")
-    print("3. View Portfolio")
-    print("4. View Stock info")
-    print("5. Logout")
+    print("3. Trending Tickers")
+    print("4. View Portfolio")
+    print("5. View Stock info")
+    print("6. Logout")
     choice = input("Enter your choice: ")
     if choice == "1":
         ticker = input("Enter the ticker of the stock: ").upper()
-        quantity = int(input("Enter the quantity of the stock: "))
-        market.buy(ticker, quantity)
-        market.view_portfolio()
+        quantity = input("Enter the quantity of the stock: ")
+        if quantity.isdigit():
+            market.buy(ticker, int(quantity))
+            market.view_portfolio()
+        else:
+            print("Invalid quantity")
+            trade_menu()
         trade_menu()
     elif choice == "2":
         ticker = input("Enter the ticker of the stock: ").upper()
-        quantity = int(input("Enter the quantity of the stock: "))
-        market.sell(ticker, quantity)
-        market.view_portfolio()
+        quantity = input("Enter the quantity of the stock: ")
+        if quantity.isdigit():
+            market.sell(ticker, int(quantity))
+            market.view_portfolio()
         trade_menu()
     elif choice == "3":
-        market.view_portfolio()
+        trading_tickers()
         trade_menu()
     elif choice == "4":
+        market.view_portfolio()
+        trade_menu()
+    elif choice == "5":
         ticker = input("Enter the ticker of the stock: ").upper()
         stock = Stock(ticker)
         print(f"Price: {stock.price}")
         print(f"Price Change: {stock.price_change}")
         stock.info()
         trade_menu()
-    elif choice == "5":
+    elif choice == "6":
         main()
 
 
